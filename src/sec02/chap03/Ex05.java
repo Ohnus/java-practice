c330f8e02702d98bab9a61aaa029f854a9afe88e
SUNHO <sh525293@gmail.com>
2026-02-11 00:21:57 +0900

    Add sec02chap03

---
+++ src/sec02/chap03/Ex05.java
@@ -0,0 +1,18 @@
+package sec02.chap03;
+
+public class Ex05 {
+    public static void main(String[] args) {
+        // 상수
+        //  💡 final 연산자 : 변수의 값을 바꾸지 못하게 만듦
+        final int INT_NUM = 1;
+        // INT_NUM = 2; // ⚠️ 불가
+
+        //  사용 가능
+        int onus, _onus, $onus, 오너스;
+
+        // 사용 불가
+//        int 1onus;
+//        int o nus;
+//        int #onus;
+    }
+}
